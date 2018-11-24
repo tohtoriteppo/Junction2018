ae186eac075e95ad46d5a2127fa8def4d0e57f32
tohtoriteppo <tohtoriteppo@hotmail.com>
2018-11-24 23:09:07 +0200

    pulling

--- VRJam/Assets/weaponMenuManager.cs
+++ VRJam/Assets/weaponMenuManager.cs
@@ -8,7 +8,7 @@ public class weaponMenuManager : MonoBehaviour
 
     private string weaponSelected;
     private float rotateSpeed = 1f;
-    private float radius = 70f;
+    private float radius = 210f;
     private Vector2 rotatePoint;
     private Object[] images;
     private List<GameObject> objects;
